3d1dd66b64866349c51d6b4d70366aa0d94c69b3
andymcgunagle <amcgunagle@gmail.com>
2022-04-05 17:27:00 -0700

    adjust footer styles.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,16 @@ import LogoAndName from "./LogoAndName";
 import styled from 'styled-components';
 
 const Wrapper = styled.footer`
-  background-color: var(--clr-brand-900);
+  background-color: var(--clr-brand-700);
   padding: var(--space-6);
+
+  & > .inner-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+     gap: var(--space-2);
+  }
 
   & * {
     color: var(--clr-white);
@@ -14,7 +22,7 @@ const Wrapper = styled.footer`
 export default function Footer() {
   return (
     <Wrapper>
-      <div className="inner-wrapper column-right">
+      <div className="inner-wrapper">
         <LogoAndName />
         <a href="https://www.andymcgunagle.com/" target="_blank" rel="noreferrer">
           Created by Andy McGunagle
